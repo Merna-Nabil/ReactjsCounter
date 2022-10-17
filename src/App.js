e6de2a70caf29c1eb11b24239d4967dea688dee6
MernaNabil2021 <merna.nabil@salam-tech.com>
2022-10-17 19:47:31 +0200

    products

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import CounterAction from "./components/CounterAction";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import About from "./components/About/Index";
+import CounterAction from "./components/counter/CounterAction";
+import Home from "./components/Home/Index";
+import Navbar from "./components/Navbar/Index";
+import Details from "./components/Shop/Details";
+import Shop from "./components/Shop/Index";
 function App() {
-
   return (
-    <div>
-      <CounterAction />
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/shop" element={<Shop />} />
+        <Route path="/counter" element={<CounterAction />} />
+        <Route path="/shop/:id" element={<Details />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/counter/CounterAction.jsx
+++ src/components/counter/CounterAction.jsx
@@ -1,16 +1,13 @@
 import { useState } from "react";
 import Counter from "./Counter";
 
-
-
 function CounterAction() {
   const [count, setCount] = useState(0);
   const increment = () => setCount(count + 1);
-  const decrrement = () => count <= 0 ? "" : setCount(count - 1);
+  const decrrement = () => (count <= 0 ? "" : setCount(count - 1));
   return (
     <div>
-      <Counter count={count} increment={increment}
-      decrrement={decrrement} />
+      <Counter count={count} increment={increment} decrrement={decrrement} />
     </div>
   );
 }
